89b53da00f18bcce9cbdf9b41ea88176cbf960ea
Carla <38359702+carlarsmendes@users.noreply.github.com>
2019-04-19 17:51:49 +0100

    Done - Adjusted after recap class

--- starter_code/app.js
+++ starter_code/app.js
@@ -10,12 +10,45 @@ app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
 
-
-
+// our index Route (GET)
 app.get('/', (req, res, next) => {
   res.render('index');
 });
 
+// // our first Route:
+// app.get('/', (request, response, next) => {
+//   response.render('home');
+// });
+
+// beers route:
+app.get('/beers', (request, response, next) => {
+  punkAPI.getBeers()
+      .then(punkBeers => {  
+        response.render('beers', {
+          beers: punkBeers
+        })
+      })
+      .catch(error => {
+        console.log("DEBUG ERROR",error)
+      });
+  })
+
+
+// random beers route:
+app.get('/random-beer', (request, response, next) => {
+  punkAPI.getRandom()
+    .then(beers => {  
+          console.log('DEBUG beers', beers)
+          //render views/one-beer.hbs" and give a variable "beer" that is beer[0]"
+          response.render('random-beer',{
+              randomBeer: beers[0]
+              })
+        })
+      .catch(error => {
+          console.log(error)
+        })
+  // response.render('random-beer');
+});
 
 
 app.listen(3000);
